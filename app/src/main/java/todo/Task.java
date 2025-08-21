c4aeaf87252d60d4e90aa0088c8f8e5f7b030b1b
Thomas-PC <langleykt@wofford.edu>
2025-08-21 12:39:15 -0400

    added documentation for task and task manager classes

--- app/src/main/java/todo/Task.java
+++ app/src/main/java/todo/Task.java
@@ -4,28 +4,59 @@ public class Task {
     private String description;
     private boolean completed;
 
+    /**
+     * Constructs a new Task with the given name and an empty description.
+     * The task is initially not completed.
+     *
+     * @param name the name of the task
+     */
     public Task(String name) {
         this(name, "");
     }
 
+    /**
+     * Constructs a new Task with the given name and description.
+     * The task is initially not completed.
+     *
+     * @param name the name of the task
+     * @param description the description of the task
+     */
     public Task(String name, String description) {
         this.name = name;
         this.description = description;
         this.completed = false;
     }
 
+    /**
+     * Returns the name of the task.
+     *
+     * @return the task name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return the task description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns whether the task is completed.
+     *
+     * @return true if the task is completed, false otherwise
+     */
     public boolean isCompleted() {
         return completed;
     }
 
+    /**
+     * Marks the task as completed.
+     */
     public void markCompleted() {
         this.completed = true;
     }
--- app/src/main/java/todo/TaskManager.java
+++ app/src/main/java/todo/TaskManager.java
@@ -1,30 +1,45 @@
 package todo;
 
 import java.util.List;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class TaskManager {
-
     private List<Task> tasks;
 
+    /**
+     * Constructs a new TaskManager with an empty list of tasks.
+     */
     public TaskManager() {
         tasks = new ArrayList<>();
         // loadTasks();
     }
 
+    /**
+     * Creates a new task with the given name and description, and adds it to the task list.
+     *
+     * @param name the name of the new task
+     * @param description the description of the new task
+     */
     public void newTask(String name, String description) {
         Task task = new Task(name, description);
         tasks.add(task);
     }
 
+    /**
+     * Adds an existing Task object to the task list.
+     *
+     * @param task the Task object to be added
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Removes the task at the specified index from the task list.
+     *
+     * @param task_index the index of the task to be removed
+     * @return true if the task was successfully removed, false if the index is invalid
+     */
     public boolean removeTask(int task_index) {
         if (task_index >= 0 && task_index < tasks.size()) {
             tasks.remove(task_index);
@@ -33,6 +48,11 @@ public class TaskManager {
         return false;
     }
 
+    /**
+     * Returns the list of all tasks.
+     *
+     * @return the list of tasks
+     */
     public List<Task> getTasks() {
         return tasks;
     }
